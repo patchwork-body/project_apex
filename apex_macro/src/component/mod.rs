b3640d27a7b7da4a4a909dcc137fa4f9d8479617
patchwork-body <personal.gugfug@gmail.com>
2025-08-06 13:30:46 +0200

    Remove unused `is_html_type` function and its module from the component structure
    - Deleted the `is_html_type.rs` file, which contained a basic implementation for checking HTML types.
    - Updated `mod.rs` to remove references to the `is_html_type` module, streamlining the component structure by eliminating unused code.

--- apex_macro/src/component/mod.rs
+++ apex_macro/src/component/mod.rs
@@ -1,10 +1,8 @@
 mod generate_component;
-mod is_html_type;
 mod parse_props;
 mod parse_slots;
 mod to_pascal_case;
 mod validate_component_function;
 
 pub(crate) use generate_component::generate_component;
-use is_html_type::is_html_type;
 use validate_component_function::validate_component_function;
